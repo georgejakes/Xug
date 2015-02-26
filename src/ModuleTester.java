4f90b3bc379baa887cccf32b74f7e8c26db78a49
Jacob <blaze@blaze-K53SM.(none)>
2015-02-26 23:10:56 +0530

    MOdule tester changed

--- src/ModuleTester.java
+++ src/ModuleTester.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 
@@ -6,7 +7,21 @@ public class ModuleTester {
 	public static void main(String args[])
 	{
 		EncodeModule enc = new EncodeModule();
+
 		System.out.print("1");
 		enc.EncodeVideo("/media/OS/aerounwired.mp4", "/home/shell/new/d.mp4", "");
-	}
-}
+	
+		
+		/*System.out.println("1");
+	PSA p=new PSA();
+	Resolution res=new Resolution(1920,1080);
+	ArrayList<Location> res1=p.psa(8, res);*/
+	
+	
+   /* while(itr.hasNext()){
+        System.out.println(itr.next().height+"  "+itr.next().width);
+
+	
+	}*/
+}}
+
